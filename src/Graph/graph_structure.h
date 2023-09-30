ce50efb54ec2a6de0a5f25fa6eb6450f9eed6d2e
LzoRn <lzr18535482977@gmail.com>
2023-09-30 16:31:06 +0900

    graph start!

---
+++ src/Graph/graph_structure.h
@@ -0,0 +1,13 @@
+//
+// Created by 75742 on 2023/9/30.
+//
+
+#ifndef LEARNALGORITHM_MAIN_GRAPH_STRUCTURE_H
+#define LEARNALGORITHM_MAIN_GRAPH_STRUCTURE_H
+namespace ltd{
+    template <typename Type>
+    struct vertex{};
+    template <typename Type>
+    struct edge{};
+}
+#endif //LEARNALGORITHM_MAIN_GRAPH_STRUCTURE_H
